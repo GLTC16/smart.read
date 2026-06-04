5ddc2fe715b3bb466a009a9946daba871ecf7617
Gianluigi Tornaghi <gianluigitornaghi@Mac.homenet.telecomitalia.it>
2026-06-04 17:03:05 +0200

    fix: whitelist google translate api in CSP

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://pagead2.googlesyndication.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data:; connect-src 'self' https://api.mymemory.translated.net https://*.supabase.co wss://*.supabase.co;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://pagead2.googlesyndication.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data:; connect-src 'self' https://api.mymemory.translated.net https://*.supabase.co wss://*.supabase.co https://translate.googleapis.com;",
           },
           {
             key: 'X-Frame-Options',
