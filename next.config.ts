39a17a3138438c79c349a8d67e750b76ee2f67ae
Gianluigi Tornaghi <gianluigitornaghi@Mac.homenet.telecomitalia.it>
2026-06-04 18:29:38 +0200

    chore: add global cache buster headers

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,18 @@ const nextConfig: NextConfig = {
             value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://pagead2.googlesyndication.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data:; connect-src 'self' blob: https://api.mymemory.translated.net https://*.supabase.co wss://*.supabase.co https://translate.googleapis.com; worker-src 'self' blob:; frame-src 'self' blob: data:;",
           },
           {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+          {
             key: 'X-Frame-Options',
             value: 'SAMEORIGIN',
           },
